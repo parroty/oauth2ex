0babce40881e7ffaf307e8c950c7be3ae61dcec3
parroty <parroty00@gmail.com>
2014-08-26 01:35:59 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OAuth2Ex.Mixfile do
 
   def project do
     [app: :oauth2ex,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 0.15.0",
      description: description,
      package: package,
