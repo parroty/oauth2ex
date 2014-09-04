060ef38c1525e7c3343cb52b79b50d0f2ce9c133
parroty <parroty00@gmail.com>
2014-09-05 01:14:58 +0900

    Fix test - specify env variables

--- test/oauth2ex_sample_test.exs
+++ test/oauth2ex_sample_test.exs
@@ -1,6 +1,15 @@
 defmodule OAuth2ExSampleTest do
   use ExUnit.Case, async: false
 
+  setup_all do
+    System.put_env("GOOGLE_API_CLIENT_ID", "test")
+    System.put_env("GOOGLE_API_CLIENT_SECRET", "test")
+    System.put_env("DROPBOX_API_CLIENT_ID", "test")
+    System.put_env("DROPBOX_API_CLIENT_SECRET", "test")
+    System.put_env("GITHUB_API_CLIENT_ID", "test")
+    System.put_env("GITHUB_API_CLIENT_SECRET", "test")
+  end
+
   test "google config" do
     assert is_map(OAuth2Ex.Sample.Google.Client.config)
   end
